91d9d62a9dad286b3f597a49368e11c6dee60e4c
Júnior Conceição <luiz.conceicao@dcomp.ufs.br>
2023-11-16 14:53:38 -0300

    Script utilizado para povoar a tabela SuperPoderes

---
+++ scripts.sql
@@ -0,0 +1,20 @@
+USE [viceri]
+GO
+
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Vôo', 'Capacidade de voar')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Invisibildade', 'Capacidade de ficar invisível')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Força', 'Poder levantar objetos pesados')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Agilidade', 'Movimentação e agilidade sobre-humanas')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Tecnologia', 'Grande disponibilidade de aparatos tecnológicos')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Telepatia', 'Capacidade de ler e controlar mentes')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Telecinese', 'Capacidade de mover objetos com o poder da mente')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Teletransporte', 'Capacidade de se transportar para qualquer lugar com o poder da mente')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Intangibilidade', 'Capacidade de atravessar objetos sólidos')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Fator de Cura', 'Capacidade de se regenerar rapidamente de qualquer ferimento')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Raio de calor', 'Capacidade de disparar rajadas de calor pelas mãos ou olhos')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Fogo', 'Capacidade de produzir e controlar fogo')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Gelo', 'Capacidade de produzir e controlar gelo')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Controle do tempo', 'Manipulação de mudanças climáticas')
+INSERT INTO [dbo].[Superpoderes] ([Superpoder], [Descricao]) VALUES ('Magnetismo', 'Possibilidade de controlar objetos de metal')
+
+GO
